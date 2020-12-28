c7f757052cf78703f4dda9a9f4dd4fb659eb3fb5
septri <09021381621129@students.ilkom.unsri.ac.id>
2020-12-28 21:46:13 +0700

    first commit

---
+++ js/apicovid.js
@@ -0,0 +1,223 @@
+const baseUrl = "https://indonesia-covid-19.mathdro.id/api";
+//const token = "6acd4debffc449c7a96ed6b6a1f72120";
+$(document).ready(() => {
+  $(".preloader-wrapper").fadeOut(3000);
+});
+// Blok kode yang akan di panggil jika fetch berhasil
+function status(response) {
+  if (response.status !== 200) {
+    console.log("Error : " + response.status);
+    // Method reject() akan membuat blok catch terpanggil
+    return Promise.reject(new Error(response.statusText));
+  } else {
+    // Mengubah suatu objek menjadi Promise agar bisa "di-then-kan"
+    return Promise.resolve(response);
+  }
+}
+// Blok kode untuk memparsing json menjadi array JavaScript
+function json(response) {
+  return response.json();
+}
+// Blok kode untuk meng-handle kesalahan di blok catch
+function error(error) {
+  // Parameter error berasal dari Promise.reject()
+  console.log("Error : " + error);
+}
+// Blok kode untuk melakukan request data json
+//ambil data 
+function getData() {
+  if ('caches' in window) {
+    caches.match(`${baseUrl}`).then((response) => {
+        if (response) {
+          response.json().then((data) => {
+            let datasHTML = "";
+            //data.forEach((datas) => {
+                datasHTML += `
+                <div class="row">
+                  <div class="col s12">
+                    <div class="card">
+                      <div class="card-content light-blue">
+                        <span class="card-title truncate">INDONESIA</span>
+                        <div class="card-panel yellow accent-2">
+                          <span class="black-text center">Confirmed : <p>${data.jumlahKasus}</p></span>
+                        </div>
+                        <div class="card-panel green accent-3">
+                          <span class="black-text center">Recovered : <p>${data.sembuh}</p></span>
+                        </div>
+                        <div class="card-panel red accent-4">
+                          <span class="black-text center">Deaths : <p color=red>${data.meninggal}</p></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              `;
+              //});
+            // Sisipkan komponen card ke dalam elemen dengan id #content
+            document.getElementById("body-content").innerHTML = datasHTML;
+          });
+        }
+      }
+    )
+  }
+  fetch(`${baseUrl}`,{
+    method: "GET",
+    /*headers: {
+      "X-Auth-Token": `${token}`
+    }*/
+  })
+    .then(status)
+    .then(json)
+    .then((data) => {
+      // Objek/array JavaScript dari response.json() masuk lewat data.
+      // Menyusun komponen card artikel match secara dinamis
+      let datasHTML = "";
+      //data.forEach((datas) => {
+          datasHTML += `
+            <div class="row">
+              <div class="col s12">
+                <div class="card">
+                  <div class="card-content light-blue">
+                    <span class="card-title truncate">INDONESIA</span>
+                    <div class="card-panel yellow accent-2">
+                      <span class="black-text center">Confirmed : <p>${data.jumlahKasus}</p></span>
+                    </div>
+                    <div class="card-panel green accent-3">
+                      <span class="black-text center">Recovered : <p>${data.sembuh}</p></span>
+                    </div>
+                    <div class="card-panel red accent-4">
+                      <span class="black-text center">Deaths : <p color=red>${data.meninggal}</p></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          `;
+       // });
+      // Sisipkan komponen card ke dalam elemen dengan id #content
+      document.getElementById("body-content").innerHTML = datasHTML;
+    })
+    .catch(error);
+}
+
+//ambil data top scorers
+function getScorers() {
+  if ('caches' in window) {
+    caches.match(`${baseUrl}/provinsi`).then((response) => {
+      if (response) {
+        response.json().then((data) => {
+            let scorersHTML = "";
+            data.data.forEach((datas) => {
+              scorersHTML += `
+                <div class="row">
+                  <div class="col s12">
+                    <div class="card">
+                      <div class="card-content light-blue">
+                        <span class="card-title truncate"><a href="./team.html?id=" black-text>Prov. ${datas.provinsi}</a></span>
+                        <div class="card-panel yellow accent-2">
+                          <span class="black-text center">Confirmed : <p>${datas.kasusPosi}</p></span>
+                        </div>
+                        <div class="card-panel green accent-3">
+                          <span class="black-text center">Recovered : <p>${datas.kasusSemb}</p></span>
+                        </div>
+                        <div class="card-panel red accent-4">
+                          <span class="black-text center">Deaths : <p color=red>${datas.kasusMeni}</p></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              `;
+            });
+            // Sisipkan komponen card ke dalam elemen dengan id #content
+            document.getElementById("body-content").innerHTML = scorersHTML;
+        });
+      }
+    })
+  }
+  fetch(`${baseUrl}/provinsi`,{
+    method: "GET",
+    /*headers: {
+      "X-Auth-Token": `${token}`
+    }*/
+  })
+    .then(status)
+    .then(json)
+    .then((data) => {
+      // Objek/array JavaScript dari response.json() masuk lewat data.
+      // Menyusun komponen card artikel match secara dinamis
+      let scorersHTML = "";
+      data.data.forEach((datas) => {
+        scorersHTML += `
+          <div class="row">
+            <div class="col s12">
+              <div class="card">
+                <div class="card-content light-blue">
+                  <span class="card-title truncate black-text">Prov. ${datas.provinsi}</span>
+                  <div class="card-panel yellow accent-2">
+                    <span class="black-text center">Confirmed : <p>${datas.kasusPosi}</p></span>
+                  </div>
+                  <div class="card-panel green accent-3">
+                    <span class="black-text center">Recovered : <p>${datas.kasusSemb}</p></span>
+                  </div>
+                  <div class="card-panel red accent-4">
+                    <span class="black-text center">Deaths : <p color=red>${datas.kasusMeni}</p></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          `;
+      });
+      // Sisipkan komponen card ke dalam elemen dengan id #content
+      document.getElementById("body-content").innerHTML = scorersHTML;
+    })
+    .catch(error);
+}
+
+function getSavedArticles() {
+    getAll().then((articles) => {
+        console.log(articles);
+        // Menyusun komponen card artikel secara dinamis
+        let articlesHTML = "";
+        articles.forEach((article) => {
+            articlesHTML += `
+              <div class="card">
+                <a href="./team.html?id=${article.id}&saved=true">
+                    <div class="card-image waves-effect waves-block waves-light">
+                      <img class="responsive-img" src="${article.crestUrl}" />
+                    </div>
+                </a>
+                <div class="card-content">
+                  <span class="card-title truncate">${article.name}</span>
+                </div>
+              </div>
+            `;
+        });
+        // Sisipkan komponen card ke dalam elemen dengan id #body-content
+        document.getElementById("body-content").innerHTML = articlesHTML;
+    })
+    .catch(error);
+}
+
+function getSavedArticleById() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var idParam = urlParams.get("id");
+    
+    getById(idParam).then((article) => {
+        articleHTML = '';
+        let articleHTML = `
+          <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="responsive-img" src="${article.crestUrl}" />
+            </div>
+            <div class="card-content">
+              <span class="card-title">${article.name}</span>
+            </div>
+          </div>
+        `;
+        // Sisipkan komponen card ke dalam elemen dengan id #content
+        document.getElementById("body-content").innerHTML = articleHTML;
+    })
+    .catch(error);
+}
---
+++ service-worker.js
@@ -0,0 +1,136 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
+if (workbox)
+  console.log(`Workbox berhasil dimuat`);
+else
+  console.log(`Workbox gagal dimuat`);
+
+//caching
+workbox.precaching.precacheAndRoute([
+  {url: '/index.html', revision: '1'},
+  {url: '/nav.html', revision: '1'},
+  {url: '/manifest.json', revision: '1'},
+  {url: '/pages/about.html', revision: '1'},
+  {url: '/pages/contact.html', revision: '1'},
+  {url: '/pages/saved.html', revision: '1'},
+  {url: '/pages/home.html', revision: '1'},
+  {url: '/pages/scorer.html', revision: '1'},
+  {url: '/css/materialize.min.css', revision: '1'},
+  {url: '/css/style.css', revision: '1'},
+  {url: '/js/materialize.min.js', revision: '1'},
+  {url: '/js/nav.js', revision: '1'},
+  {url: '/js/db.js', revision: '1'},
+  {url: '/js/apicovid.js', revision: '1'},
+  {url: '/js/idb.js', revision: '1'},
+]);
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 hari
+      }),
+    ],
+  })
+);
+
+/*workbox.routing.registerRoute(
+  new RegExp('/team.html?'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'konten-team'
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('/player.html?'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'konten-payer'
+  })
+);*/
+
+workbox.routing.registerRoute(
+  new RegExp('/pages/'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'pages'
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://indonesia-covid-19.mathdro.id/api/'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'api-data'
+  })
+);
+
+/*workbox.routing.registerRoute(
+  new RegExp('https://crests.football-data.org/'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'api-img',
+    plugins: [
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);*/
+
+// Menyimpan cache dari JQuery
+workbox.routing.registerRoute(
+  /^https:\/\/code\.jquery\.com/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'jquery-min-js',
+  })
+);
+
+// Menyimpan cache dari CSS Google Fonts
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+);
+ 
+// Menyimpan cache untuk file font selama 1 tahun
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+
+//push
+self.addEventListener('push', (event) => {
+    var body;
+    if (event.data) {
+      body = event.data.text();
+    } else {
+      body = 'Push message no payload';
+    }
+    var options = {
+      body: body,
+      icon: '/assets/images/icons/icon-72x72.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: 1
+      }
+    };
+    event.waitUntil(
+      self.registration.showNotification('Covid19 PWA', options)
+    );
+});
